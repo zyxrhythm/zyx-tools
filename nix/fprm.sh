bb81f799020d7f6a934359c0626cee83209d7f70
Zyx Rhythm <zyxrhythm@gmail.com>
2020-06-17 17:45:51 +0800

    Update fprm.sh

--- nix/fprm.sh
+++ nix/fprm.sh
@@ -6,7 +6,7 @@
 #$3 = file permission (if blank, default value: 644)
 #$4 = recursive switch ( if 'r' is added as 4th parameter the script will do a recursive file perm fix)
 
-
+# 1st
 if [ -z $1 ]; then 
 tardir="./"
 elif [ ! -z $1 ] && [ ! -d $1 ]; then
@@ -19,37 +19,57 @@ fi
 
 permregex="^[0-7]{3}$"
 
-if [ -z $2 ] || [ $2 = x ]; then 
+# 2nd
+if [ -z $2 ]; then 
 dirperm=755
 
 elif [ ! -z $2 ] && [[ $2 =~ $permregex ]]; then 
 dirperm=$2
 
+elif [ ! -z $2 ] && [ $2 = x ]; then
+dskip=y
+
 else 
 dirperm=755
 fi
 
-if [ -z $3 ] || [ $3 = x ]; then 
-dirperm=644
+# 3rd
+if [ -z $3 ]; then 
+fileperm=644
 
 elif [ ! -z $3 ] && [[ $3 =~ $permregex ]]; then 
-dirperm=$3
+fileperm=$3
+
+elif [ ! -z $3 ] && [ $3 = x ]; then
+fskip=y
 
 else 
-dirperm=644
+fileperm=644
 fi
 
-
+# 4th
 if [ $4 = r ]; then
-
-find $tardir -type d -exec chmod $dirperm {} \;
-find $tardir -type f -exec chmod $fileperm {} \;
+ 
+ if [ $dskip != y ]; then
+ find $tardir -type d -exec chmod $dirperm {} \;
+ fi
+ 
+ if [ $fskip != y ]; then
+ find $tardir -type f -exec chmod $fileperm {} \;
+ fi
 
 else
+
 tardir="."
 
-find $tardir -type d -exec chmod $dirperm {} \;
-find $tardir -type f -exec chmod $fileperm {} \;
+ if [ $dskip != y ]; then
+ find $tardir -type d -exec chmod $dirperm {} \;
+ fi
+ 
+ if [ $fskip != y ]; then
+ find $tardir -type f -exec chmod $fileperm {} \;
+ fi
+
 fi
 
 exit 0
