a00e1cc91b1cc1f3ecb03b779a795f85649f58d4
Zyx Rhythm <zyxrhythm@gmail.com>
2020-06-17 17:24:11 +0800

    Update fprm.sh

--- nix/fprm.sh
+++ nix/fprm.sh
@@ -4,6 +4,7 @@
 #$1 = target (if blank, default value: current directory)
 #$2 = directory permission (if blank, default value: 755)
 #$3 = file permission (if blank, default value: 644)
+#$4 = recursive switch ( if 'r' is added as 4th parameter the script will do a recursive file perm fix)
 
 
 if [ -z $1 ]; then 
@@ -16,21 +17,39 @@ else
 tardir=$1
 fi
 
-if [ -z $2 ]; then 
+permregex="^[0-7]{3}$"
+
+if [ -z $2 ] || [ $2 = x ]; then 
 dirperm=755
-else
+
+elif [ ! -z $2 ] && [[ $2 =~ $permregex ]]; then 
 dirperm=$2
+
+else 
+dirperm=755
 fi
 
-if [ -z $3 ]; then 
-fileperm=644
-else
-fileperm=$3
+if [ -z $3 ] || [ $3 = x ]; then 
+dirperm=644
+
+elif [ ! -z $3 ] && [[ $3 =~ $permregex ]]; then 
+dirperm=$3
+
+else 
+dirperm=644
 fi
 
+
+if [ $4 = r ]; then
 
 find $tardir -type d -exec chmod $dirperm {} \;
 find $tardir -type f -exec chmod $fileperm {} \;
 
+else
+tardir="."
+
+find $tardir -type d -exec chmod $dirperm {} \;
+find $tardir -type f -exec chmod $fileperm {} \;
+fi
 
 exit 0
