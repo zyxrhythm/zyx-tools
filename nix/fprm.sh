9a6993efd6382e404a7d75375b77cd663255754f
Zyx Rhythm <zyxrhythm@gmail.com>
2020-06-17 21:06:04 +0800

    Update fprm.sh

--- nix/fprm.sh
+++ nix/fprm.sh
@@ -1,75 +1,54 @@
 #!/bin/bash
 #Fix permissions of files and folder
 #This script is based on this blog: https://odd.blog/2013/11/05/fix-file-644-directory-775-permissions-linux-easily/
-#$1 = target (if blank, default value: current directory)
-#$2 = directory permission (if blank, default value: 755)
-#$3 = file permission (if blank, default value: 644)
-#$4 = recursive switch ( if 'r' is added as 4th parameter the script will do a recursive file perm fix)
+#$1 = directory permission (if blank, default value: 755, use 'x' to prevent the script from changing directory permissions)
+#$2 = file permission (if blank, default value: 644, use x to to prevent the script from changing file permissions)
+#$3 = recursive switch ( if 'r' is added as 3rd parameter the script will do a recursive file perm fix on the current dir, but if a valid is used as 3rd parameter, the script will update permissions on that directory)
 
 # 1st
-if [ -z $1 ]; then 
-tardir="./"
-elif [ ! -z $1 ] && [ ! -d $1 ]; then
-echo "First parameter should be a valid directory!"
-exit 1
-
-else
-tardir=$1
-fi
-
 permregex="^[0-7]{3}$"
+
+if [ -z $1 ]; then 
+dirperm='755'
+
+elif [ ! -z $1 ] && [[ $1 =~ $permregex ]]; then 
+dirperm=$1
+
+elif [ ! -z $1 ] && [ $1 = 'x' ]; then
+dskip='y'
+
+fi
 
 # 2nd
 if [ -z $2 ]; then 
-dirperm=755
+fileperm='644'
 
 elif [ ! -z $2 ] && [[ $2 =~ $permregex ]]; then 
-dirperm=$2
+fileperm=$2
 
-elif [ ! -z $2 ] && [ $2 = x ]; then
-dskip=y
+elif [ ! -z $2 ] && [ $2 = 'x' ]; then
+fskip='y'
 
-else 
-dirperm=755
 fi
 
 # 3rd
-if [ -z $3 ]; then 
-fileperm=644
 
-elif [ ! -z $3 ] && [[ $3 =~ $permregex ]]; then 
-fileperm=$3
+if [ -d $3 ]; then
+tardir="$3"
+ 
+elif [ $3 = 'r' ]; then
+tardir="./"
 
-elif [ ! -z $3 ] && [ $3 = x ]; then
-fskip=y
-
-else 
-fileperm=644
+elif [ -z $3 ]; then
+tardir="."
 fi
 
-# 4th
-if [ $4 = r ]; then
+if [[ $dskip != 'y' ]]; then
+find $tardir -type d -exec chmod $dirperm {} \;
+fi
  
- if [ $dskip != y ]; then
- find $tardir -type d -exec chmod $dirperm {} \;
- fi
- 
- if [ $fskip != y ]; then
- find $tardir -type f -exec chmod $fileperm {} \;
- fi
-
-else
-
-tardir="."
-
- if [ $dskip != y ]; then
- find $tardir -type d -exec chmod $dirperm {} \;
- fi
- 
- if [ $fskip != y ]; then
- find $tardir -type f -exec chmod $fileperm {} \;
- fi
-
+if [[ $fskip != 'y' ]]; then
+find $tardir -type f -exec chmod $fileperm {} \;
 fi
 
 exit 0
