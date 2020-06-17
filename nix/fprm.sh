74382a235cb783c266740b0905d34386df0283a0
Zyx Rhythm <zyxrhythm@gmail.com>
2020-06-17 21:24:53 +0800

    Update fprm.sh

--- nix/fprm.sh
+++ nix/fprm.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-#Fix permissions of files and folder
+#Fix permissions of files and folders
 #This script is based on this blog: https://odd.blog/2013/11/05/fix-file-644-directory-775-permissions-linux-easily/
 #$1 = directory permission (if blank, default value: 755, use 'x' to prevent the script from changing directory permissions)
 #$2 = file permission (if blank, default value: 644, use x to to prevent the script from changing file permissions)
-#$3 = recursive switch ( if 'r' is added as 3rd parameter the script will do a recursive file perm fix on the current dir, but if a valid is used as 3rd parameter, the script will update permissions on that directory)
+#$3 = recursive switch ( if 'r' is added as 3rd parameter the script will do a recursive file perm fix on the current dir, but if a valid is used as 3rd parameter, the script will update permissions on that directory, if left blank the script will do a non-recursive file and dir perm fix on the current dir)
+
+if [ $1 = 'help' ]; then
+clear
+echo "
+
+Fix permissions of files and folders
+
+This script is based on the following blog: 
+https://odd.blog/2013/11/05/fix-file-644-directory-775-permissions-linux-easily/
+
+\$1 = directory permission 
+     if blank, default value: 755 
+     use 'x' to prevent the script from modifying directory permissions
+	 
+\$2 = file permission
+     if blank, default value: 644, 
+     use x to to prevent the script from modifying file permissions.
+	 
+\$3 = recursive switch
+     if 'r' is added as 3rd parameter the script will do a recursive file and dir perm fix on the current dir, 
+     if a valid directtory is used as 3rd parameter, the script will update permissions on that directory.
+     if left blank the script will do a non-recursive file and dir perm fix on the current dir
+
+"
+
+else 
 
 # 1st
 permregex="^[0-7]{3}$"
@@ -52,3 +78,5 @@ find $tardir -type f -exec chmod $fileperm {} \;
 fi
 
 exit 0
+
+fi
