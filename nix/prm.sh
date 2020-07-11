b7fa8835b88114c20d5352835ced816226bd3ecb
Zyx Rhythm <zyxrhythm@gmail.com>
2020-07-11 09:21:16 +0800

    Update prm.sh

--- nix/prm.sh
+++ nix/prm.sh
@@ -6,7 +6,7 @@ echo "
 
 This script is basically orbiting the 'stat' command. The script shows files and folders permission on 'octal number' format.
 
-You can use it by simply typing 'prm' followed by the path to the file/directory. Typing 'prm -h' adds hidden files/folders that starts with '.' on the results. And 'prm -x' shall include the owner of the file/folder in the result. If you do not include a path to file/folder the script will be executed on your current working directory.
+You can use it by simply typing 'prm' followed by the path to the file/directory. Typing 'prm -h' adds hidden files/folders that starts with '.' on the results. And 'prm -x' shall include the "user and group" that owns of the file/folder in the result. If you do not include a path to file/folder the script will be executed on your current working directory.
 
     sample syntax is as follows:
 
