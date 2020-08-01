ea057c5adddcb51cf3f5769d3e177e5c079aa8ae
root <zyxrhythm@gmail.com>
2020-08-01 11:46:16 +0000

    update

--- nix/zyxw-dip.sh
+++ nix/zyxw-dip.sh
@@ -49,6 +49,17 @@ then
 	echo -e "\nwhich command not found. please install which."
 fi
 
+if [[ $whoisprog = whois && ! -e /etc/whois.conf && ! -d /etc/whois.conf ]]
+then 
+echo -e "\n/etc/whois.conf not found!\n\nPlease create a whois.conf\n\nIf you do not create a whois.conf,\nthe scipt might produce unwanted results or no results at all.\n\nYou can get the recommended whois.conf at:\nhttps://github.com/zyxrhythm/zyx-tools/tree/master/conf\n"
+exit 1
+
+elif [[ $whoisprog = jwhois && ! -e /etc/whois.conf && ! -d /etc/whois.conf ]]
+then 
+echo -e "\n/etc/jwhois.conf not found!\n\nPlease create a whois.conf\n\nIf you do not create a jwhois.conf,\nthe scipt might produce unwanted results or no results at all.\n\nYou can get the recommended jwhois.conf at:\nhttps://github.com/zyxrhythm/zyx-tools/tree/master/conf\n"
+exit 1
+fi
+
 #list of program that needs to be installed
 proglist=$( echo -e "dig\ngawk\nsed\nnslookup")
 
@@ -66,12 +77,12 @@ do
 done < <(echo "$1" )
 }
 
-pcresult=$(progcheckfunc "$proglist" )
+progcheckresult=$(progcheckfunc "$proglist" )
 
 #terminates the script if one of the pcresult is not blank, idicating that a program is missing
 if [[ ! -z $pcresult ]]
 then
-echo "$pcresult"
+echo "$progcheckresult"
 exit 1
 fi
 
