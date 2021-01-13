fbe9a21b8f0c9644e69c993d6782302c977eab72
ZYX Rhythm <zyxrhythm@gmail.com>
2021-01-14 12:49:14 +1300

    Update zyxw-dip.sh

--- nix/zyxw-dip.sh
+++ nix/zyxw-dip.sh
@@ -952,11 +952,23 @@ else
 	dsnzfuncr=$( dsnzfunc "$(echo "$zyx" | grep -i "query_status:" )" )
 
 	lastmod=$(echo "$zyx" | grep -i "domain_datelastmodified:")
+	
+	#stores the domain registration date
+	regdate=$(echo "$zyx" | grep -i -e "domain_dateregistered:")
 
+	#stores the domain creation date
+	credate=$(echo "$zyx" | grep -i -e "domain_datecreated:")
+
+	#stores deleagaterequested value on variable
+	delreq=$(echo "$zyx" | grep -i -e "domain_delegaterequested:")
+
+	#stores signed value
+	domsig=$(echo "$zyx" | grep -i -e "domain_signed:")
+	
 	nameservers=$(echo "$zyx" | grep -i "ns_name_.*")
 	nsfrctnz=$( nsfunction "$nameservers")
 
-	echo -e "\n__________________________\n\nDomain Name: $domain\nRegistrar: ${registrar#*:}\nRegistrar Country: ${regcoun#*:}\n__________________________\nDomain Status:\n$dsnzfuncr\n--------------------------\nLast Modified: ${lastmod#*:}\n__________________________\n$nsfrctnz\n__________________________\n$arfrctnz\n__________________________\n$mrfrctnz\n__________________________\n"
+	echo -e "\n__________________________\n\nDomain Name: $domain\nRegistrar: ${registrar#*:}\nRegistrar Country: ${regcoun#*:}\n__________________________\nDomain Status:\n$dsnzfuncr\n--------------------------\nRegistration Date: $regdate\nCreation Date: $credate\nLast Modified: ${lastmod#*:}\nDelegate Requested: $delreq\nDomain Signed: $domsig\n__________________________\n$nsfrctnz\n__________________________\n$arfrctnz\n__________________________\n$mrfrctnz\n__________________________\n"
 	;;
 
 	#special whois result trim for UK TLDs
